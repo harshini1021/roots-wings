248cf075f8f6414003fa5b5b75bb4cd9de08b1bd
harshini1021 <guriharshini10@gmail.com>
2026-06-14 06:46:50 +0530

    remove nested folder

--- roots-wings-final/backend/src/routes/alumni.routes.js
+++
@@ -1,7 +0,0 @@
-const r = require('express').Router();
-const c = require('../controllers/alumni.controller');
-const { protect } = require('../middleware/auth.middleware');
-r.get('/stats', protect, c.getStats);
-r.get('/', protect, c.getAll);
-r.get('/:id', protect, c.getOne);
-module.exports = r;
--- roots-wings-final/backend/src/routes/stories.routes.js
+++
@@ -1,6 +0,0 @@
-const r = require('express').Router();
-const c = require('../controllers/stories.controller');
-const { protect } = require('../middleware/auth.middleware');
-r.get('/', protect, c.getAll);
-r.post('/', protect, c.create);
-module.exports = r;
